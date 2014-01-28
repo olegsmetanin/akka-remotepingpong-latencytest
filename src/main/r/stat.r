4c7e4edb4809e6f09f5b27f46796b3f2ebff7a6e
olegsmith <olegsmith@gmail.com>
2014-01-29 03:39:34 +0400

    improve plots

--- src/main/r/stat.r
+++ src/main/r/stat.r
@@ -11,29 +11,30 @@ for (i in 1:length(fi$filename)) {
 
     data <- read.table(toString(fi$filename[i]), header=TRUE, sep=",")
 
+    data$roundtime <- as.integer(data$time)
+
+    rpsframe <- aggregate( data$roundtime, list(data$roundtime), FUN = length)
+
+    colnames(rpsframe) <- c("roundtime", "freq")
+
+    data <- merge(data, rpsframe, by="roundtime", all=TRUE)
+
     concurrency <- fi$conc[i]
 
-    rps <- max(data$time)/length(data$time)
+    rps <- round(length(data$time)/max(data$time), digits = 0)
 
-    quant <- quantile(data$latency, c(0.99))
+    quant <- quantile(data$latency, c(0.99))[1]
 
-    baselatency <- subset(data, latency <= quant[1], select=c(latency))
+    roundquant <- round(quant, digits = 2)
 
-    slowlatency <- subset(data, latency > quant[1], select=c(latency))
+    baselatency <- subset(data, latency <= quant, select=c(latency))
+
+    slowlatency <- subset(data, latency > quant, select=c(latency))
 
     png(paste("./stat/plot",toString(concurrency),".png", sep = ""), width = 1000, height = 1000, res=120)
 
     par(mfrow=c(3,2),xpd=NA, oma = c( 4, 0, 4, 0 ) )
 
-    ## Latency in time
-    plot(
-        data$time,
-        data$latency,
-        type="l",
-        main="Latency in time",
-        ylab="Latency, ms",
-        xlab="Time from start, s"
-    )
 
     ## Histogram of remote actor latency, 99% of requests
     hist(
@@ -41,11 +42,12 @@ for (i in 1:length(fi$filename)) {
         main = "Histogram of remote actor latency, 99% of requests",
         xlab = "Latency, ms",
         yaxt = "n",
-        sub = paste("requests latency <= ",toString(quant[1])," ms", sep = ""),
+        sub = paste("requests latency <= ",toString(roundquant)," ms", sep = ""),
         breaks = 9,
         labels = TRUE,
         freq = TRUE
     )
+
     axis(
         side = 2,
         at=axTicks(2),
@@ -57,15 +59,6 @@ for (i in 1:length(fi$filename)) {
     count <- paste("count = ", length(baselatency$latency))
     legend(x = "topright", c(leg1,leg2,count), bty = "n")
 
-    ## Pong actor mailbox size in time
-    plot(
-        data$time,
-        data$pongrcvmailboxsize,
-        type="l",
-        main="Pong actor mailbox size in time",
-        ylab="Mailbox size",
-        xlab="Time from start, s"
-    )
 
     ## Histogram of remote actor latency, 1% of requests
     hist(
@@ -73,7 +66,7 @@ for (i in 1:length(fi$filename)) {
         main="Histogram of remote actor latency, 1% of requests",
         xlab="Latency, ms",
         yaxt="n",
-        sub=paste("requests latency > ",toString(quant[1])," ms", sep = ""),
+        sub=paste("requests latency > ",toString(roundquant)," ms", sep = ""),
         labels=TRUE,
         freq = TRUE
     )
@@ -88,6 +81,38 @@ for (i in 1:length(fi$filename)) {
     legend(x = "topright", c(count), bty = "n")
 
 
+    ## Latency in time
+    plot(
+        data$time,
+        data$latency,
+        type="l",
+        main="Latency in time",
+        ylab="Latency, ms",
+        xlab="Time from start, s"
+    )
+
+    ## Requests per second
+    plot(
+        data$time,
+        data$freq,
+        type="l",
+        main="Requests per second",
+        ylab="rps",
+        xlab="Time from start, s"
+    )
+
+
+    ## Pong actor mailbox size in time
+    plot(
+        data$time,
+        data$pongrcvmailboxsize,
+        type="l",
+        main="Pong actor mailbox size in time",
+        ylab="Mailbox size",
+        xlab="Time from start, s"
+    )
+
+
     ## Ping actor mailbox size in time
     plot(
         data$time,
@@ -97,6 +122,7 @@ for (i in 1:length(fi$filename)) {
         ylab="Mailbox size",
         xlab="Time from start, s"
     )
+
 
     ## Main title
     title(
@@ -113,7 +139,9 @@ for (i in 1:length(fi$filename)) {
             "concurrency = ",
             toString(concurrency),
             ", count = ",
-            toString(length(data$latency))
+            toString(length(data$latency)),
+            ", rps = ",
+            toString(rps)
         )
     )
 
@@ -130,9 +158,22 @@ for (i in 1:length(fi$filename)) {
 
 }
 
-png(paste("./stat/plot.png"), width = 600, height = 600, res = 120)
+alldata$roundtime <- as.integer(alldata$time)
+
+rpsframe <- aggregate( id ~ concurrency+roundtime,data = alldata,FUN=length )
+
+colnames(rpsframe) <- c("concurrency", "roundtime", "freq")
+
+alldata <- merge(alldata, rpsframe, by=c("roundtime","concurrency"), all=TRUE)
+
+png(paste("./stat/plot.png"), width = 600, height = 1000, res = 120)
+
+par(mfrow=c(2,1))
 
 boxplot(latency~concurrency,data=alldata, main="Latency versus Concurrency",
   	 xlab="Concurrency, number of parallel requests", ylab="Latency, ms", log = "y", outline=FALSE)
+
+boxplot(freq~concurrency,data=alldata, main="Rps versus Concurrency",
+  	 xlab="Concurrency, number of parallel requests", ylab="Rps", outline=FALSE)
 
 dev.off()
